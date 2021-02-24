e227d0623f0a026cd505ce3d30759bff0935b3d5
Chris Doyle <cdoyle@temple.edu>
2021-02-24 09:21:06 -0500

    restore sprockets

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,10 +3,9 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
+// require("@rails/ujs").start()
+// require("turbolinks").start()
+// require("@rails/activestorage").start()
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -16,5 +15,5 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-require("trix")
-require("@rails/actiontext")
+// require("trix")
+// require("@rails/actiontext")
