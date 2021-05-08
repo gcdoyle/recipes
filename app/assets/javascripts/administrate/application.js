6c70a39f2777c9c3b23fca8f6d32adf79d2916ae
Chris Doyle <cdoyle@temple.edu>
2021-05-08 09:08:36 -0400

    Prevent accidental submission while entering ingredients.

--- app/assets/javascripts/administrate/application.js
+++ app/assets/javascripts/administrate/application.js
@@ -16,3 +16,11 @@ addEventListener("trix-initialize", event => {
   inputElement.type = "text"
   inputElement.pattern = "(https?://|/).+"
 })
+
+$("#recipe-form").on("keyup keypress", function(e) {
+  var keyCode = e.keyCode || e.which;
+  if (keyCode === 13) { 
+    e.preventDefault();
+    return false;
+  }
+});
